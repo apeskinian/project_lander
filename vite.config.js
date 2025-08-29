beecc57edec34129059f84261cb37aea5550b5c6
Matthew Aitken <matt@itken.net>
2025-08-29 16:47:28 +0100

    Install coverage

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
   server: {
-    host: true, // binds to 0.0.0.0 so other devices can access
-    port: 5173, // or whatever port you prefer
+    host: true,
+    port: 5173,
   },
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+    },
   },
-}))
+});
