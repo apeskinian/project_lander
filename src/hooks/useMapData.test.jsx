d6f6802d17e90fe331d59118b8da261e5d1e551a
Matthew Aitken <matt@itken.net>
2025-08-31 19:39:54 +0100

    Add tests for useMapData

---
+++ src/hooks/useMapData.test.jsx
@@ -0,0 +1,48 @@
+import { renderHook, waitFor } from '@testing-library/react';
+import { useMapData } from './useMapData';
+
+describe('useMapData', () => {
+    beforeEach(() => {
+        global.fetch = vi.fn();
+    });
+    afterEach(() => {
+        vi.resetAllMocks();
+    });
+    it('should fetch map data successfully', async () => {
+        // arrange
+        const mockData = { data: { name: 'test' } };
+        (fetch).mockResolvedValueOnce({
+            json: async () => mockData,
+        });
+        const { result } = renderHook(() => useMapData());
+        // assert
+        await waitFor(() => {
+            expect(result.current.loading).toBe(false);
+        });
+        expect(result.current.mapData).toEqual(mockData.data);
+        expect(result.current.error).toBe(null);
+    });
+    it('should handle fetch error', async () => {
+        // arrange
+        (fetch).mockRejectedValueOnce(new Error('error'));
+        const { result } = renderHook(() => useMapData());
+        //assert
+        await waitFor(() => {
+            expect(result.current.loading).toBe(false);
+        });
+        expect(result.current.mapData).toBe(null);
+        expect(result.current.error).toBeInstanceOf(Error);
+    });
+    it('should handle no data is found', async () => {
+        // arrange
+        (fetch).mockResolvedValueOnce({
+            json: async () => ({}),
+        });
+        const { result } = renderHook(() => useMapData());
+        // assert
+        await waitFor(() => {
+            expect(result.current.loading).toBe(false);
+        });
+        expect(result.current.mapData).toBe(null);
+    });
+});
